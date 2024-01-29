984b5f7d62acb0a7699e76a5390e49b4088a7a73
Ethiotech <henokaklilu16@gmail.com>
2024-01-29 20:52:01 +0300

    update sign up page

--- App/signup.php
+++ App/signup.php
@@ -39,6 +39,14 @@ if (isset($_SESSION['errorMsg'])) {
               <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" required>
           </div>
           <div class="form-group">
+                 <label for="stream">Stream</label>
+                 <select name="Stream" class="stream">
+                    <option value="Gnr" >General(9-10)</option>
+                    <option value="scoial">Scoial</option>
+                    <option value="Natural" selected>Natural</option>
+                 </select>
+          </div>
+          <div class="form-group">
               <label for="password">Password</label>
               <input type="password" id="password" name="password" placeholder="Enter your password" required>
           </div>
@@ -46,14 +54,7 @@ if (isset($_SESSION['errorMsg'])) {
               <label for="confirmPassword">Confirm Password</label>
               <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password" required>
           </div>
-          <div class="form-group">
-                 <label for="stream">Stream</label>
-                 <select name="Stream" id="stream">
-                    <option value="Gnr" >General(9-10)</option>
-                    <option value="scoial">Scoial</option>
-                    <option value="Natural" selected>Natural</option>
-                 </select>
-          </div>
+         
           <button type="submit" class="signup-button">Sign Up</button>
       </form>
       <a href="index.php" class="login-link">Already have an account? Login here</a>
